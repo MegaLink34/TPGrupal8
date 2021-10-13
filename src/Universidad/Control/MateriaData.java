67bbb44b708f324719c703cd2c06366c4a856292
Agostina-Fernandez <fernandez_agostina96@hotmail.com>
2021-10-13 18:02:50 -0300

    ACTUALIZACION ARCHIVOS DATA
    - borrados lógicos con sus respectivas activaciones

--- src/Universidad/Control/MateriaData.java
+++ src/Universidad/Control/MateriaData.java
@@ -114,4 +114,89 @@ public class MateriaData {
         
         return materias;
     }
+    
+    public void borrarMateria(int id){
+        String comandoSql = "DELETE * FROM materia WHERE idMateria=?";
+        try {
+            PreparedStatement prepStat = conexion.prepareStatement(comandoSql);
+            
+            prepStat.setInt(1, id);
+            
+            prepStat.executeUpdate();
+            
+            prepStat.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al eliminar Materia");
+        }
+    }
+    
+    public List<Materia> obtenerMateriasActivas(){
+        String comandoSql = "SELECT * FROM materia WHERE activo=true";
+        List<Materia> materias = new ArrayList<>();
+        Materia materia = null;
+        
+        try {
+            PreparedStatement prepStat = conexion.prepareStatement(comandoSql);
+            
+            ResultSet resultSet = prepStat.executeQuery();
+            
+            while (resultSet.next()){
+                
+                materia = new Materia();
+                
+                materia.setIdMateria(resultSet.getInt("idMateria"));
+                materia.setNombre(resultSet.getString("nombre"));
+                materia.setAnio(resultSet.getInt("anio"));
+                materia.setActivo(resultSet.getBoolean("activo"));
+                
+                materias.add(materia);
+                
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al buscar");
+        }
+        
+        return materias;
+    }
+    
+    public void activarMateria(int id){
+        String comandoSql = "UPDATE materia activo=? WHERE idMateria=?";
+        PreparedStatement prepStat;
+        
+        try {
+            prepStat = conexion.prepareStatement(comandoSql);
+            
+            prepStat.setBoolean(1, true);
+            
+            prepStat.setInt(2, id);
+            
+            prepStat.executeUpdate();
+            
+            prepStat.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al activar");
+        }
+    }
+    
+    public void desactivarMateria(int id){
+        String comandoSql = "UPDATE materia activo=? WHERE idMateria=?";
+        PreparedStatement prepStat;
+        
+        try {
+            prepStat = conexion.prepareStatement(comandoSql);
+            
+            prepStat.setBoolean(1, false);
+            
+            prepStat.setInt(2, id);
+            
+            prepStat.executeUpdate();
+            
+            prepStat.close();
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al desactivar");
+        }
+    }
 }
